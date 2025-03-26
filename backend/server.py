54f987ce2ac4d0beb7f6c06e2dc0f38c5fdd0ffe
thecreativetalha <110997703+MuhammadTalhaWaheed@users.noreply.github.com>
2025-03-27 03:03:05 +0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -295,4 +295,5 @@ def get_i_score():
         return jsonify({"error": f"Error fetching i_score: {str(e)}"}), 500
        
 if __name__ == '__main__':
-    app.run(host='172.17.41.194', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Get PORT from Render
+    app.run(host='0.0.0.0', port=port, debug=True)
